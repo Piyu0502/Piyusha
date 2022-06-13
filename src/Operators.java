d9a3d58fffa76f1c736a4f5a9fd38dc68294fbb1
Piyu0502 <piyushamore4848@gmail.com>
2022-06-13 16:13:02 +0530

    if statements

--- src/Operators.java
+++ src/Operators.java
@@ -31,7 +31,19 @@ public class Operators {
 
         System.out.println(10<<2); //10*2^2 = 10 * 4 = 40
         System.out.println(20<<2); //20*2^2 = 20 * 4 = 80
+        System.out.println(10>>2); //10/2^2 = 10/4 = 2
+        System.out.println(20>>3); //20/2^3 =20/8 = 2
 
+        //Logical operator
+
+        System.out.println(c<y&&y<c);// True && false = false
+        System.out.println(c<y&z<y); // true & false
+        System.out.println(c<y||y<z); // true || true = true
+        System.out.println(c<y|y<z); //true | true = true
+
+        //Ternary Operator
+
+        
 
     }
 
