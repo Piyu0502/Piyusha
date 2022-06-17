77ffd8aebc53a0cc983dc2d4dde4e7ff681833d9
Piyu0502 <piyushamore4848@gmail.com>
2022-06-17 15:22:07 +0530

    initial commit

---
+++ src/controlflowstatement/OddEvenEX.java
@@ -0,0 +1,11 @@
+package controlflowstatement;
+
+public class OddEvenEX {
+    public static void main(String[] args){
+        int i = 5;
+        if(i%2==0)
+            System.out.println("Given number is Even");
+        else
+            System.out.println("Given number is Odd");
+    }
+}
