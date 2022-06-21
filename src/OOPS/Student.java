e7219c6be84dd39aae36b735bbbe08a5375ef1b1
Piyu0502 <piyushamore4848@gmail.com>
2022-06-21 16:07:29 +0530

    initial commit

---
+++ src/OOPS/Student.java
@@ -0,0 +1,44 @@
+package OOPS;
+import java.util.Scanner;
+
+public class Student {
+    int rno;
+    String name;
+    String address;
+    float age;
+    void insertData(int r,String nm, String addr, float a) {
+        rno = r;
+        name = nm;
+        address = addr;
+        age = a;
+    }
+    void display(){
+        System.out.println("!!!!!!!!! STUDENT DETAILS !!!!!!!!!!");
+        System.out.println("Roll no:"+rno);
+        System.out.println("Name:"+name);
+        System.out.println("Address:"+address);
+        System.out.println("Age:"+age);
+    }
+}
+    class StudentImpl {
+        public static void main(String[] args) {
+            Student s1 = new Student();  // s1 is reference variable
+            s1.rno = 1;
+            s1.name = "Amit";
+            s1.address = "Kothrud ,pune";
+            s1.age = 4.5f;
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter rno:");
+            int rno = sc.nextInt();
+            System.out.println("Enter name:");
+            String nm = sc.next();
+            System.out.println("Enter Address");
+            String addr = sc.next();
+            System.out.println("Enter age");
+            float age = sc.nextFloat();
+            Student s2 = new Student();
+            s2.insertData(rno,nm,addr,age);
+            s2.display();
+        }
+    }
--- src/controlflowstatement/Star4.java
+++ src/controlflowstatement/Star4.java
@@ -1,12 +1,16 @@
 package controlflowstatement;
 
 public class Star4 {
-    public static void main(String[] args){
-        for(int i=1; i<=5; i++) {
-            for (int j=5; j>=i; j--) {
-                System.out.print("* ");
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 3; j >= i; j--) {
+                System.out.print("");
             }
-            System.out.println();
+                    for (int k = 1; k <= i; k++){
+                System.out.print(" *");
         }
+            System.out.println();
     }
 }
+}
+
