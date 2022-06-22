a9d9b8c8283fa5630faf07c1e0d94493a29c588c
Piyu0502 <piyushamore4848@gmail.com>
2022-06-22 10:09:38 +0530

    initial commit

--- src/OOPS/Student.java
+++ src/OOPS/Student.java
@@ -1,4 +1,5 @@
 package OOPS;
+
 import java.util.Scanner;
 
 public class Student {
@@ -6,39 +7,42 @@ public class Student {
     String name;
     String address;
     float age;
-    void insertData(int r,String nm, String addr, float a) {
+
+    void insertData(int r, String nm, String addr, float a) {
         rno = r;
         name = nm;
         address = addr;
         age = a;
     }
-    void display(){
+
+    void display() {
         System.out.println("!!!!!!!!! STUDENT DETAILS !!!!!!!!!!");
-        System.out.println("Roll no:"+rno);
-        System.out.println("Name:"+name);
-        System.out.println("Address:"+address);
-        System.out.println("Age:"+age);
+        System.out.println("Roll no:" + rno);
+        System.out.println("Name:" + name);
+        System.out.println("Address:" + address);
+        System.out.println("Age:" + age);
     }
 }
-    class StudentImpl {
-        public static void main(String[] args) {
-            Student s1 = new Student();  // s1 is reference variable
-            s1.rno = 1;
-            s1.name = "Amit";
-            s1.address = "Kothrud ,pune";
-            s1.age = 4.5f;
 
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter rno:");
-            int rno = sc.nextInt();
-            System.out.println("Enter name:");
-            String nm = sc.next();
-            System.out.println("Enter Address");
-            String addr = sc.next();
-            System.out.println("Enter age");
-            float age = sc.nextFloat();
-            Student s2 = new Student();
-            s2.insertData(rno,nm,addr,age);
-            s2.display();
-        }
+class StudentImpl {
+    public static void main(String[] args) {
+        Student s1 = new Student();  // s1 is reference variable
+        s1.rno = 1;
+        s1.name = "Amit";
+        s1.address = "Kothrud ,pune";
+        s1.age = 4.5f;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rno:");
+        int rno = sc.nextInt();
+        System.out.println("Enter name:");
+        String nm = sc.next();
+        System.out.println("Enter Address");
+        String addr = sc.next();
+        System.out.println("Enter age");
+        float age = sc.nextFloat();
+        Student s2 = new Student();
+        s2.insertData(rno, nm, addr, age);
+        s2.display();
     }
+}
---
+++ src/controlflowstatement/StarInitialP.java
@@ -0,0 +1,20 @@
+package controlflowstatement;
+
+public class StarInitialP {
+    public static void main(String[] args) {
+        int i, j, k;
+        for (i = 0; i <= 11; i++) {
+            for (j = 0; j <= 2; j++) {
+                System.out.print("*");
+            }
+            for (k = 1; k <= 5; k++) {
+                if (i >= 0 && i < 2 || i > 4 && i < 7 || i >= 2 && i <= 4 && k == 5) {
+                    System.out.print("**");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
