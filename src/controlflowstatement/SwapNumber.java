6e1f1b462600aa91cb3c6448eefa45d2d18222da
Piyu0502 <piyushamore4848@gmail.com>
2022-06-26 10:19:18 +0530

    initial commit

--- src/controlflowstatement/SwapNumber.java
+++ src/controlflowstatement/SwapNumber.java
@@ -1,14 +1,14 @@
 package controlflowstatement;
 
 public class SwapNumber {
-    public static void main(String[] args){
-        int a=10;
-        int b=30;
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
         System.out.println("!!!!!!Swap two numbers using two variables!!!!!!!");
         a = a + b;
         b = a - b;
         a = a - b;
-        System.out.println("a :" +a);
-        System.out.println("b :" +b);
+        System.out.println("a :" + a);
+        System.out.println("b :" + b);
     }
 }
