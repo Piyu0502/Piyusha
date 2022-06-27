d17cf79431d268ba2b2aaca723d7343525df04d9
Piyu0502 <piyushamore4848@gmail.com>
2022-06-27 15:56:18 +0530

    initial commit

---
+++ src/Association/Vehicle.java
@@ -0,0 +1,50 @@
+package Association;
+
+public class Vehicle {
+    int wheels;
+    String vehicleType;
+    String brand;
+}
+
+class Car {
+    public String vehicleType;
+    public String brand;
+    String segment;
+    String model;
+    String variant;
+    Engine engine;             //aggregation (created the object)
+    MPlayer mPlayer;
+}
+
+class Engine {
+    int stroke;
+    String engineType;
+    int cc;
+}
+
+class MPlayer {
+    String companyName;
+    double price;
+}
+
+class VehicleImpl {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.brand = "Suzuki";
+        car.vehicleType = "Car";
+        car.model = "Swift";
+        car.segment = "Hashback";
+        car.variant = "Diesel";
+
+        Engine suzukiEngine = new Engine();
+        suzukiEngine.engineType = "BS6";
+        suzukiEngine.cc = 1296;
+        suzukiEngine.stroke = 4;
+        car.engine = suzukiEngine;
+        MPlayer sonyPlayer = new MPlayer();
+        sonyPlayer.companyName = "Sony";
+        sonyPlayer.price = 3400;
+        car.mPlayer = sonyPlayer;
+    }
+}
+
