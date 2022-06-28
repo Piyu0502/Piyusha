12d2f33e2c23b2707a6ba89b77ceec89cc74fa41
Piyu0502 <piyushamore4848@gmail.com>
2022-06-28 11:38:26 +0530

    initial commit

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -1,11 +1,11 @@
 public class Datatypes {
     public static void main(String []args){
-        int a = 10;
-
-
-
-
-
+        boolean flag = false;
+        byte b = 0;
+        short s = 0;
+        int a = 0;
+        long l = 0;
+        float f = 0.0f;
+        double d = 0.0d;
     }
-
 }
