59590b77d8cfcd808f9756c446f6f4f2061dedee
Piyu0502 <piyushamore4848@gmail.com>
2022-07-13 16:03:10 +0530

    initial commit

---
+++ src/Array/ArrayDemo1.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        int[] a1; // instantiation
+        //initialization
+        a1 = new int[10];
+        a1[0] = 10;
+        a1[1] = 20;
+        a1[2] = 30;
+        a1[3] = 40;
+        a1[4] = 50;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a1[i] + "");
+        }
+    }
+}
