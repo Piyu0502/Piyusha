0568691e113e65bb95834a4876883edbf8e2a752
Piyu0502 <piyushamore4848@gmail.com>
2022-08-02 19:01:19 +0530

    initial commit

---
+++ src/ExceptionHandling/CheckedExceptionDemo.java
@@ -0,0 +1,27 @@
+package ExceptionHandling;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CheckedExceptionDemo {
+    void m1() throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream("abc.txt");
+    }
+
+    void m2() throws FileNotFoundException{
+        m1();
+    }
+    void m3() throws FileNotFoundException{     //suppress
+        m2();
+    }
+
+    public static void main(String[] args) {
+        CheckedExceptionDemo  demo = new CheckedExceptionDemo();
+        try {
+            demo.m3();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Rest of the Code");
+    }
+}
