c539889bd2989b2c3c12b316bf04439d0206eb27
Piyu0502 <piyushamore4848@gmail.com>
2022-08-11 11:54:11 +0530

    initial commit

---
+++ src/JDBC/MySqlDeleteDemo.java
@@ -0,0 +1,21 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MySqlDeleteDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/Employee";
+        String username = "root";
+        String password = "Kbjoshi@123";
+        final String QUERY =  "delete from emp where empno = ?";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, password);
+        PreparedStatement ps = con.prepareStatement(QUERY);
+        ps.setInt(1,121);
+        int no = ps.executeUpdate();
+        System.out.println(no + "Row/s Affected");
+    }
+}
---
+++ src/JDBC/MysqlSelectDemo.java
@@ -0,0 +1,25 @@
+package JDBC;
+
+import java.sql.*;
+
+public class MysqlSelectDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/Employee";
+        String username = "root";
+        String password = "Kbjoshi@123";
+        final String SELECT_QUERY = "select * from emp;";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, password);  //Static method
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(SELECT_QUERY);
+        System.out.println("Id " + " Name " + " Job " + " MGR ");
+        while (rs.next()) {
+            System.out.print(rs.getInt(1) + " ");
+            System.out.print(rs.getString(2) + " ");
+            System.out.print(rs.getString(3) + " ");
+            System.out.println(rs.getString("mgr"));
+        }
+
+    }
+
+}
