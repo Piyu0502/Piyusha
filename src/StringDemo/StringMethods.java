ac5a0895359bedadbd8c28d8f210df55bfaebf7f
Piyu0502 <piyushamore4848@gmail.com>
2022-08-17 10:24:10 +0530

    initial commit

--- src/StringDemo/StringMethods.java
+++ src/StringDemo/StringMethods.java
@@ -3,11 +3,13 @@ package StringDemo;
 public class StringMethods {
     public static void main(String[] args) {
         String str = "Codekul, Codekul";
-        System.out.println("Upper Case: " +str.toUpperCase());
-        System.out.println("Lower Case: " +str.toLowerCase());
-        System.out.println("Char At: " +str.charAt(3));
-        System.out.println("Concat: "+str.concat("Pvt . Ltd."));
-        System.out.println("Replace: "+str.replace(str,"Mumbai"));
-        System.out.println("Replace All: "+str.replaceAll("Codekul","Mumbai"));
+        System.out.println("Upper Case: " + str.toUpperCase());
+        System.out.println("Lower Case: " + str.toLowerCase());
+        System.out.println("Char At: " + str.charAt(3));
+        System.out.println("Concat: " + str.concat("Pvt . Ltd."));
+        System.out.println("Replace: " + str.replace(str, "Mumbai"));
+        System.out.println("Replace All: " + str.replaceAll("Codekul", "Mumbai"));
+        System.out.println("Contains "+str.contains("brings"));
+        System.out.println(str);
     }
 }
