b6116baa71ead4f69c661eae22012d023a00f7ff
Piyu0502 <piyushamore4848@gmail.com>
2022-09-14 18:48:46 +0530

    initial commit

---
+++ src/FileHandling/FileMethods.java
@@ -0,0 +1,25 @@
+package FileHandling;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileMethods {
+    public static void main(String[] args) {
+        File file = new File("codekul.txt");
+        try {
+            if (file.createNewFile())
+                System.out.println("File is Created");
+            else{
+                System.out.println("File Exists");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("IS FILE" + file.isFile());
+        System.out.println("PATH" +file.getPath());
+        System.out.println(file.getName());
+
+    }
+
+}
